b5f837efc371d824926611d877fbb75e0e56ec92
Loïc <loic@jeedom.com>
2016-11-02 17:01:42 +0100

    Improve UI

--- desktop/php/mode.php
+++ desktop/php/mode.php
@@ -45,26 +45,22 @@ foreach ($eqLogics as $eqLogic) {
 }
 ?>
 </div>
-
 </div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-
   <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
   <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-
-
+  <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
   <ul class="nav nav-tabs" role="tablist">
+    <li><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
     <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
     <li role="presentation"><a href="#modetab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Modes}}</a></li>
   </ul>
-
-
   <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
     <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+      <br/>
       <form class="form-horizontal">
         <fieldset>
-          <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
           <div class="form-group">
             <label class="col-sm-3 control-label">{{Nom de l'équipement mode}}</label>
             <div class="col-sm-3">
@@ -108,8 +104,7 @@ foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
  </form>
 </div>
 <div role="tabpanel" class="tab-pane" id="modetab">
-  <a class="btn btn-success pull-right" id="bt_addMode" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter mode}}</a>
-  <br/><br/>
+  <a class="btn btn-success pull-right" id="bt_addMode" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter mode}}</a><br/><br/>
   <div id="div_modes"></div>
 </div>
 </div>
