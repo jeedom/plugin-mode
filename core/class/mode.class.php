fb550512272fd4a58b8c68eea49f0137afb1a404
Jean-François Auger <nechry@gmail.com>
2017-07-11 21:23:27 +0200

    use mode log file

--- core/class/mode.class.php
+++ core/class/mode.class.php
@@ -105,7 +105,7 @@ class mode extends eqLogic {
 						}
 						scenarioExpression::createAndExec('action', $action['cmd'], $options);
 					} catch (Exception $e) {
-						log::add('alarm', 'error', __('Erreur lors de l\'éxecution de ', __FILE__) . $action['cmd'] . __('. Détails : ', __FILE__) . $e->getMessage());
+						log::add('mode', 'error', __('Erreur lors de l\'éxecution de ', __FILE__) . $action['cmd'] . __('. Détails : ', __FILE__) . $e->getMessage());
 					}
 				}
 				return;
