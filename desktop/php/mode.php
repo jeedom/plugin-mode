218a135eee757a101ddc0b70e9af5caab282cda2
JEALG <jalain.gosse@icloud.com>
2024-01-13 10:56:44 +0100

    suppression double barre de recherche

--- desktop/php/mode.php
+++ desktop/php/mode.php
@@ -33,13 +33,6 @@ $eqLogics = eqLogic::byType($plugin->getId());
 			?>
 		</div>
 		<legend><i class="fas fa-th-large"></i> {{Mes Modes}}</legend>
-		<div class="input-group" style="margin:5px;">
-			<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
-			<div class="input-group-btn">
-				<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i>
-				</a><a class="btn hidden roundedRight" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>
-			</div>
-		</div>
 		<?php
 		if (count($eqLogics) == 0) {
 			echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement Mode trouvé, cliquer sur "Ajouter" pour commencer}}</div>';
